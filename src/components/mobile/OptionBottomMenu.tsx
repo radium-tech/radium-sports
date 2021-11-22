158845476ee8a0f2b8ed753dba5d346dd8e36ba8
Pedro Manoel <dev.pedromanoel@pm.me>
2021-11-22 15:55:10 -0300

    feat: add a new component
    This is the Option Bottom Menu component. It represents each option of the bottom menu on mobile
    screens.

---
+++ src/components/mobile/OptionBottomMenu.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/dist/client/router';
+import OptionBottomMenuModel from '../../../model/optionBottomMenu';
+import styles from '../../styles/components/OptionBottomMenu.module.css';
+import updateOption from '../../../functions/optionsBottomMenu';
+
+interface OptionBottomMenuProps {
+  option: OptionBottomMenuModel;
+}
+
+export default function OptionBottomMenu({ option }: OptionBottomMenuProps) {
+  const [getOption, setOption] = useState(option);
+  const router = useRouter();
+  const currentPath = router.asPath;
+
+  function renderOption() {
+    const selectedOption = getOption.selected ? styles.selected : '';
+    return (
+      <div className={`${styles.option} ${selectedOption}`}>
+        <a href={getOption.link}>
+          <div className={styles.icon}>
+            <img src={getOption.iconPath} alt="" />
+          </div>
+        </a>
+      </div>
+    );
+  }
+  useEffect(() => {
+    setOption(updateOption(getOption, currentPath));
+    renderOption();
+  }, [router.asPath]);
+
+  return renderOption();
+}
