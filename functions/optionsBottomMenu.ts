ba3a981526b4ad1bf77160d6fdc53f3831ce8f2c
Pedro Manoel <dev.pedromanoel@pm.me>
2021-11-26 16:02:19 -0300

    chore: delete older folders

--- functions/optionsBottomMenu.ts
+++
@@ -1,8 +0,0 @@
-import OptionBottomMenuModel from '../model/optionBottomMenu';
-
-export default function updateOption(option: OptionBottomMenuModel, currentPath: string) {
-  if (option.link === currentPath) {
-    return new OptionBottomMenuModel(option.title, option.iconPath, option.link, true);
-  }
-  return option;
-}
