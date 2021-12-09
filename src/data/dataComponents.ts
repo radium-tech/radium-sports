4da0644f3c7f66403b604c3d6fafc8fc55906bf3
Heitor Moura <heitorandrade2001@gmail.com>
2021-12-09 15:08:01 -0300

    chore: add components data variables file

---
+++ src/data/dataComponents.ts
@@ -0,0 +1,10 @@
+const SidebarData = [
+  { title: 'Todas', path: '/' },
+  { title: 'Futebol', path: '/futebol' },
+  { title: 'Basquete', path: '/basquete' },
+  { title: 'Vôlei', path: '/volei' },
+  { title: 'eSports', path: '/esports' },
+  { title: 'UFC', path: '/ufc' },
+];
+
+export default SidebarData;
